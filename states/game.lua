894a58c4e5f8594dbcb8386de6aa8c5c2d4f7c9e
joshalexjacobs <joshalexjacobs@gmail.com>
2016-12-13 17:31:27 -0500

    player can now move the circle by swiping

--- states/game.lua
+++ states/game.lua
@@ -2,17 +2,18 @@
 game = {}
 
 local circle = {
-  x = 75,
-  y = 75,
+  x = 70,
+  y = 125,
   r = 20, -- may be too small
   color = {225, 225, 225, 255},
 
   -- our circle's collision box
   rect = {
-    x = 50,
-    y = 50,
+    x = 0,
+    y = 0,
     w = 50,
     h = 50,
+    pad = 5, -- padding
     draw = "line",
     color = {0, 0, 0, 255},
     touched = false
@@ -37,6 +38,20 @@ end
 
 function game:touchreleased(id, x, y, dx, dy, pressure)
   if circle.rect.touched then
+    if x > circle.rect.x + circle.rect.w then -- move right
+      circle.x = circle.x + 50
+      circle.rect.x = circle.x - circle.r - circle.rect.pad
+    elseif x < circle.rect.x then -- move left
+      circle.x = circle.x - 50
+      circle.rect.x = circle.x - circle.r - circle.rect.pad
+    elseif y > circle.rect.y + circle.rect.h then -- move down
+      circle.y = circle.y + 50
+      circle.rect.y = circle.y - circle.r - circle.rect.pad
+    elseif y < circle.rect.y then -- move up
+      circle.y = circle.y - 50
+      circle.rect.y = circle.y - circle.r - circle.rect.pad
+    end
+
     circle.rect.touched = false
   end
 end
@@ -46,7 +61,9 @@ function game:touchmoved(id, x, y, dx, dy, pressure)
 end
 
 function game:enter()
-
+  -- set up circle
+  circle.rect.x = circle.x - circle.r - circle.rect.pad
+  circle.rect.y = circle.y - circle.r - circle.rect.pad
 end
 
 function game:update(dt)
@@ -63,9 +80,15 @@ function game:draw()
 
   -- play area
   love.graphics.setColor(0, 0, 0, 150)
-  love.graphics.rectangle("line", 50, 50, 540, 540) -- outline
+  love.graphics.rectangle("line", 45, 100, 550, 550) -- outline 540 x 540 1 circle is 50 x 50
   love.graphics.setColor({225, 225, 225, 30})
-  love.graphics.rectangle("fill", 50, 50, 540, 540)
+  love.graphics.rectangle("fill", 45, 100, 550, 550)
+
+  -- invisible grid
+  for i = 1, 10 do
+    love.graphics.rectangle("line", 45 + i * 50, 100, 1, 550)
+    love.graphics.rectangle("line", 45, 100 + i * 50, 550, 1)
+  end
 
   -- object collision space
   love.graphics.setColor(circle.rect.color)
@@ -75,6 +98,8 @@ function game:draw()
   love.graphics.setColor(circle.color)
   love.graphics.circle("fill", circle.x, circle.y, circle.r)
 
-  -- invisible grid
-  --love.graphics.rectangle("line", 40, 0, 1, 1136)
+  love.graphics.setColor(circle.rect.color)
+  if circle.x < circle.y then lowest = circle.x else lowest = circle.y end
+  love.graphics.line(circle.x, circle.y, circle.x - lowest, circle.y - lowest)
+
 end
